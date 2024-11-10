f2c728ec82a9c0897461c7b588976888e66820f2
Nils Bars <nils.bars@rub.de>
2024-11-10 20:59:19 +0000

    Add vagrant docker container wrapper script

---
+++ vagrant.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run -it --rm \
+        -e LIBVIRT_DEFAULT_URI \
+        -v /var/run/libvirt/:/var/run/libvirt/ \
+        -v ~/.vagrant.d:/.vagrant.d \
+        -v $(realpath "${PWD}"):${PWD} \
+        -w "${PWD}" \
+        --network host \
+        vagrantlibvirt/vagrant-libvirt:latest \
+        vagrant $*
