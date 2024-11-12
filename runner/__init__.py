6e9e44d51bc35c71f12d2a773050be3cf057287d
Nils Bars <nils.bars@rub.de>
2024-11-12 08:12:55 +0000

    Import DockerRunnerWithoutOverlayfs in runner module

--- runner/__init__.py
+++ runner/__init__.py
@@ -1,3 +1,3 @@
 from .plain import DefaultAflRunner
-from .docker import DefaultDockerRunner
+from .docker import DefaultDockerRunner, DockerRunnerWithoutOverlayfs
 from .vm import DefaultVmRunner, DefaultVmRunnerV2
